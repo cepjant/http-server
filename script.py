ae991c2974520ebfd50d95dd127aae5e7a00912b
cepjant <vernigor191919@gmail.com>
2020-09-25 00:15:34 +1000

    финал

--- script.py
+++ script.py
@@ -8,30 +8,27 @@ hostName = "localhost"
 serverPort = 8000
 
 
-
-
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
-        print(self.path)
         if self.path.startswith('/city/'):
             self.get_city_info()
         elif self.path.startswith('/cities/?p='):
             self.get_all_cities()
         elif self.path.startswith('/cities/?compare='):
-            self.get_compare()
+            self.get_compared()
+        elif self.path.startswith('/cities/?search='):
+            self.search()
         else:
             self.send_headers(404)
             response = {"status": "not found"}
             self.write_html(str(response))
 
-
-    def send_headers(self, code): # вынес сюда чтобы не дублировать в каждой функции
+    def send_headers(self, code):  # вынес сюда чтобы не дублировать в каждой функции
         self.send_response(code)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
-
-    def write_html(self, body): # вынес сюда чтобы не дублировать в каждой функции
+    def write_html(self, body):  # вынес сюда чтобы не дублировать в каждой функции
         self.wfile.write(bytes("<html><head><meta content='text/html;  \
             charset=utf-8' http-equiv='Content-Type'></head>", "utf-8"))
         self.wfile.write(bytes("<meta content='utf-8' http-equiv='encoding'>  \
@@ -40,9 +37,8 @@ class Server(BaseHTTPRequestHandler):
         self.wfile.write(bytes(body, "utf-8"))
         self.wfile.write(bytes("</body>", "utf-8"))
 
-
     def convert_into_json(self, city_info):
-        data = {
+        json_data = {
             "geonameid": city_info[0],
             "name": city_info[1],
             "asciiname": city_info[2],
@@ -56,21 +52,20 @@ class Server(BaseHTTPRequestHandler):
             "admin1 code": city_info[10],
             "admin2 code": city_info[11],
             "admin3 code": city_info[12],
-            "admin4 code":  city_info[13],
+            "admin4 code": city_info[13],
             "population": city_info[14],
             "elevation": city_info[15],
             "dem": city_info[16],
             "timezone": city_info[17],
             "modification date": city_info[18].strip(),
         }
-        if len(city_info) > 18:
-            data.update({
+        if len(city_info) > 19:
+            json_data.update({
                 'is_norther': city_info[19],
                 'has_different_tz': city_info[20],
                 'tz_diff': city_info[21]
-                })
-        return data
-
+            })
+        return json_data
 
     def get_city_info(self):
         city_id = self.path.split('/')[-1]
@@ -80,16 +75,15 @@ class Server(BaseHTTPRequestHandler):
                 city_info = i
         if city_info:
             response = {
-            'data': self.convert_into_json(city_info),
-            'status': 'ok'}
+                'data': self.convert_into_json(city_info),
+                'status': 'ok'}
             self.send_headers(200)
             self.write_html(str(response))
         else:
             response = {
-            'status': 'not found'}
+                'status': 'not found'}
             self.send_headers(404)
             self.write_html(str(response))
-
 
     def get_all_cities(self):
         if int(self.path.split('=')[-1]) < 1:
@@ -98,103 +92,110 @@ class Server(BaseHTTPRequestHandler):
         result = data[page * 10: page * 10 + 10]
         if result:
             response = {
-            "data": [self.convert_into_json(city) for city in result],
-            "status": "ok"}
+                "data": [self.convert_into_json(city) for city in result],
+                "status": "ok"}
             self.send_headers(200)
             self.write_html(str(response))
         else:
             response = {
-            "status": "not found"}
+                "status": "not found"}
             self.send_headers(404)
             self.write_html(str(response))
 
-
-    def get_compare(self):
+    def get_compared(self):
         cities = self.path[self.path.find('=') + 1:].split(',')
-        city_1_req = urllib.parse.unquote(cities[0]) # правим кодировку
-        city_2_req = urllib.parse.unquote(cities[1])
+        city_1_req = urllib.parse.unquote(cities[0]).strip()  # правим кодировку
+        city_2_req = urllib.parse.unquote(cities[1]).strip()
         result = []
         city_1_matches = []
         city_2_matches = []
         for city in data:
             alternatenames = city[3].split(',')
-            if city_1_req in alternatenames:
-                city_1_matches.append(city)
-            if city_2_req in alternatenames:
-                city_2_matches.append(city)
-        city_1 = sorted(city_1_matches, key=lambda i: int(i[14]), reverse=True)[0]
-        city_2 = sorted(city_2_matches, key=lambda i: int(i[14]), reverse=True)[0]
-        if float(city_1[4]) > float(city_2[4]):
-            city_1.append("True")
-            city_2.append('False')
-        elif float(city_1[4]) < float(city_2[4]):
-            city_1.append("False")
-            city_2.append('True')
-        
-        if city_1[17] != city_2[17]:
-            city_1.append('True')
-            city_2.append('True')
-            IST_1 = pytz.timezone(city_1[17])
-            IST_2 = pytz.timezone(city_2[17])
-            time_in_city_1 = datetime.now(IST_1)
-            time_in_city_2 = datetime.now(IST_2)
-            first_time = int(str(time_in_city_1).split(' ')[1][-5:-3])
-            second_time = int(str(time_in_city_2).split(' ')[1][-5:-3])
-            if first_time > second_time:
-                time_difference = first_time - second_time
-                city_1.append('+' + str(time_difference))
-                city_2.append('-' + str(time_difference))
+            if city_1_req.lower() in [i.lower() for i in alternatenames]:
+                city_1_matches.append(city.copy())
+            if city_2_req.lower() in [i.lower() for i in alternatenames]:
+                city_2_matches.append(city.copy())
+        if city_2_matches and city_1_matches:
+            city_1 = sorted(city_1_matches, key=lambda i: int(i[14]), reverse=True)[0] # сорт по population
+            city_2 = sorted(city_2_matches, key=lambda i: int(i[14]), reverse=True)[0]
+            if float(city_1[4]) > float(city_2[4]):
+                city_1.append("True")
+                city_2.append('False')
+            elif float(city_1[4]) < float(city_2[4]):
+                city_1.append("False")
+                city_2.append('True')
+
+            if city_1[17] != city_2[17]:
+                city_1.append('True')
+                city_2.append('True')
+                IST_1 = pytz.timezone(city_1[17])
+                IST_2 = pytz.timezone(city_2[17])
+                time_in_city_1 = datetime.now(IST_1)
+                time_in_city_2 = datetime.now(IST_2)
+                first_tz = int(str(time_in_city_1).split(' ')[1][-5:-3])
+                second_tz = int(str(time_in_city_2).split(' ')[1][-5:-3])
+                if first_tz > second_tz:
+                    time_difference = first_tz - second_tz
+                    city_1.append('+' + str(time_difference))
+                    city_2.append('-' + str(time_difference))
+                else:
+                    time_difference = second_tz - first_tz
+                    city_1.append('-' + str(time_difference))
+                    city_2.append('+' + str(time_difference))
             else:
-                time_difference = second_time - first_time
-                city_1.append('-' + str(time_difference))
-                city_2.append('+' + str(time_difference))     
-        else:
-            city_1.append('False')
-            city_1.append(0)
-            city_2.append('False')
-            city_2.append(0)
-        print(city_1)
-        print(city_2)
-        result += city_1, city_2
-        
-        if result:
+                city_1.append('False')
+                city_1.append(0)
+                city_2.append('False')
+                city_2.append(0)
+            result += city_1, city_2
+
             response_list = []
             for city in result:
                 json_data = self.convert_into_json(city)
                 response_list.append(json_data)
             response = {
-            "data": response_list,
-            "status": "ok"}
+                "data": response_list,
+                "status": "ok"}
             self.send_headers(200)
             self.write_html(str(response))
-            print(response)
-            print(response_list)
         else:
             response = {
-            "status": "not found"}
+                "status": "not found"}
             self.send_headers(404)
             self.write_html(str(response))
 
-
-
-
-
-
-
+    def search(self):
+        request = urllib.parse.unquote(self.path.split('=')[-1])
+        result = []
+        for city in data:
+            alternatenames = city[3].split(',')
+            for name in alternatenames:
+                if name.lower().startswith(request.lower()):
+                    result.append(name)
+        if result:
+            response = {
+                'data': result[:20],
+                'status': 'ok'}
+            self.send_headers(200)
+            self.write_html(str(response))
+        else:
+            response = {
+                'status': 'not found'}
+            self.send_headers(404)
+            self.write_html(str(response))
 
 
 if __name__ == "__main__":
     data = []
     try:
         with open('RU.txt', 'r') as f:
-            data_list= f.readlines()
+            data_list = f.readlines()
             for d in data_list:
                 data.append(d.split('\t'))
             print('Данные загружены')
 
-    except FileNotFoundError: # перепроверить
+    except FileNotFoundError:  # перепроверить
         print('Ошибка! Файл "RU.txt" с данными не найден в текущей директории.')
-
 
     webServer = HTTPServer((hostName, serverPort), Server)
     print("Server started http://%s:%s" % (hostName, serverPort))
